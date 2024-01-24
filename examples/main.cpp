074432ce10835790b09c36ef07f44e42299ce76c
Christian <christianstrodl1@gmail.com>
2024-01-24 11:07:44 +1000

    Loads gltf and converts to meshlets

--- examples/main.cpp
+++ examples/main.cpp
@@ -2,9 +2,29 @@
 #include <Canta/SDLWindow.h>
 #include <Canta/RenderGraph.h>
 #include <Canta/ImGuiContext.h>
+#include <Canta/UploadBuffer.h>
 #include <Cen/Engine.h>
 
-int main() {
+#include <fastgltf/parser.hpp>
+#include <fastgltf/types.hpp>
+#include <fastgltf/tools.hpp>
+#include <meshoptimizer.h>
+#include <stack>
+
+template <>
+struct fastgltf::ElementTraits<ende::math::Vec3f> : fastgltf::ElementTraitsBase<ende::math::Vec3f, AccessorType::Vec3, float> {};
+template <>
+struct fastgltf::ElementTraits<ende::math::Vec<2, f32>> : fastgltf::ElementTraitsBase<ende::math::Vec<2, f32>, AccessorType::Vec2, float> {};
+template <>
+struct fastgltf::ElementTraits<ende::math::Vec4f> : fastgltf::ElementTraitsBase<ende::math::Vec4f, AccessorType::Vec4, float> {};
+
+int main(int argc, char* argv[]) {
+
+    std::filesystem::path gltfPath = {};
+    if (argc > 1)
+        gltfPath = argv[1];
+    else
+        return -1;
 
     canta::SDLWindow window("Cen Main", 1920, 1080);
 
@@ -23,13 +43,176 @@ int main() {
         .device = engine.device(),
         .window = &window
     });
+    auto uploadBuffer = canta::UploadBuffer::create({
+        .device = engine.device(),
+        .size = 1 << 16
+    });
 
-    std::array<canta::CommandPool, canta::FRAMES_IN_FLIGHT> commandPools = {};
-    for (auto& pool : commandPools) {
-        pool = engine.device()->createCommandPool({
-            .queueType = canta::QueueType::GRAPHICS
-        }).value();
+
+    fastgltf::Parser parser;
+    fastgltf::GltfDataBuffer data;
+    data.loadFromFile(gltfPath);
+    auto type = fastgltf::determineGltfFileType(&data);
+    auto asset = type == fastgltf::GltfType::GLB ?
+            parser.loadGltfBinary(&data, gltfPath.parent_path(), fastgltf::Options::None) :
+            parser.loadGltf(&data, gltfPath.parent_path(), fastgltf::Options::LoadGLBBuffers | fastgltf::Options::LoadExternalBuffers);
+
+    if (auto error = asset.error(); error != fastgltf::Error::None) {
+        return -2;
     }
+
+    struct Vertex {
+        ende::math::Vec3f position = {};
+        ende::math::Vec<2, f32> uvs = {};
+        ende::math::Vec3f normal = {};
+    };
+    std::vector<Vertex> vertices = {};
+    std::vector<u32> indices = {};
+    struct Meshlet {
+        u32 vertexOffset = 0;
+        u32 indexOffset = 0;
+        u32 indexCount = 0;
+        u32 primitiveOffset = 0;
+    };
+    std::vector<Meshlet> meshlets = {};
+    std::vector<u8> primitives = {};
+
+    struct Mesh {
+        u32 meshletOffset = 0;
+        u32 meshletCount = 0;
+    };
+    std::vector<Mesh> meshes = {};
+
+    struct NodeInfo {
+        u32 assetIndex = 0;
+    };
+    std::stack<NodeInfo> nodeInfos = {};
+    for (u32 index : asset->scenes.front().nodeIndices) {
+        nodeInfos.push({
+            .assetIndex = index
+        });
+    }
+
+    while (!nodeInfos.empty()) {
+        auto [ assetIndex ] = nodeInfos.top();
+        nodeInfos.pop();
+
+        auto& assetNode = asset->nodes[assetIndex];
+
+        for (u32 child : assetNode.children) {
+            nodeInfos.push({
+                .assetIndex = child
+            });
+        }
+
+        if (!assetNode.meshIndex.has_value())
+            continue;
+        u32 meshIndex = assetNode.meshIndex.value();
+        auto& assetMesh = asset->meshes[meshIndex];
+        for (auto& primitive : assetMesh.primitives) {
+            u32 firstVertex = vertices.size();
+            u32 firstIndex = indices.size();
+            u32 firstMeshlet = meshlets.size();
+            u32 firstPrimitive = primitives.size();
+
+            auto& indicesAccessor = asset->accessors[primitive.indicesAccessor.value()];
+            u32 indexCount = indicesAccessor.count;
+
+            std::vector<Vertex> meshVertices = {};
+            std::vector<u32> meshIndices(indexCount);
+            fastgltf::iterateAccessorWithIndex<u32>(asset.get(), indicesAccessor, [&](u32 index, u32 idx) {
+                meshIndices[idx] = index;
+            });
+
+            if (auto positionsIt = primitive.findAttribute("POSITION"); positionsIt != primitive.attributes.end()) {
+                auto& positionsAccessor = asset->accessors[positionsIt->second];
+                meshVertices.resize(positionsAccessor.count);
+                fastgltf::iterateAccessorWithIndex<ende::math::Vec3f>(asset.get(), positionsAccessor, [&](ende::math::Vec3f position, u32 idx) {
+                    meshVertices[idx].position = position;
+                });
+            }
+
+            if (auto uvsIt = primitive.findAttribute("TEXCOORD_0"); uvsIt != primitive.attributes.end()) {
+                auto& uvsAccessor = asset->accessors[uvsIt->second];
+                meshVertices.resize(uvsAccessor.count);
+                fastgltf::iterateAccessorWithIndex<ende::math::Vec<2, f32>>(asset.get(), uvsAccessor, [&](ende::math::Vec<2, f32> uvs, u32 idx) {
+                    meshVertices[idx].uvs = uvs;
+                });
+            }
+
+            if (auto normalsIt = primitive.findAttribute("NORMAL"); normalsIt != primitive.attributes.end()) {
+                auto& normalsAccessor = asset->accessors[normalsIt->second];
+                meshVertices.resize(normalsAccessor.count);
+                fastgltf::iterateAccessorWithIndex<ende::math::Vec3f>(asset.get(), normalsAccessor, [&](ende::math::Vec3f normal, u32 idx) {
+                    meshVertices[idx].normal = normal;
+                });
+            }
+
+            const u32 maxVertices = 64;
+            const u32 maxTriangles = 64;
+            const u32 coneWeight = 0.f;
+
+            u32 maxMeshlets = meshopt_buildMeshletsBound(meshIndices.size(), maxVertices, maxTriangles);
+            std::vector<meshopt_Meshlet> meshoptMeshlets(maxMeshlets);
+            std::vector<u32> meshletIndices(maxMeshlets * maxVertices);
+            std::vector<u8> meshletPrimitives(maxMeshlets * maxTriangles * 3);
+
+            u32 meshletCount = meshopt_buildMeshlets(meshoptMeshlets.data(), meshletIndices.data(), meshletPrimitives.data(), meshIndices.data(), meshIndices.size(), (f32*)meshVertices.data(), meshVertices.size(), sizeof(Vertex), maxVertices, maxTriangles, coneWeight);
+
+            auto& lastMeshlet = meshoptMeshlets[meshletCount - 1];
+            meshletIndices.resize(lastMeshlet.vertex_offset + lastMeshlet.vertex_count);
+            meshletPrimitives.resize(lastMeshlet.triangle_offset + ((lastMeshlet.triangle_count * 3 + 3) & ~3));
+            meshoptMeshlets.resize(meshletCount);
+
+            std::vector<Meshlet> meshMeshlets;
+            meshMeshlets.reserve(meshletCount);
+            for (auto& meshlet : meshoptMeshlets) {
+                meshMeshlets.push_back({
+                    .vertexOffset = firstVertex,
+                    .indexOffset = meshlet.vertex_offset + firstIndex,
+                    .indexCount = meshlet.vertex_count,
+                    .primitiveOffset = meshlet.triangle_offset + firstPrimitive
+                });
+            }
+
+            vertices.insert(vertices.end(), meshVertices.begin(), meshVertices.end());
+            indices.insert(indices.end(), meshletIndices.begin(), meshletIndices.end());
+            primitives.insert(primitives.end(), meshletPrimitives.begin(), meshletPrimitives.end());
+            meshlets.insert(meshlets.end(), meshMeshlets.begin(), meshMeshlets.end());
+
+            meshes.push_back({
+                .meshletOffset = firstMeshlet,
+                .meshletCount = static_cast<u32>(meshMeshlets.size())
+            });
+        }
+    }
+
+    auto vertexBuffer = engine.device()->createBuffer({
+        .size = static_cast<u32>(vertices.size() * sizeof(Vertex)),
+        .usage = canta::BufferUsage::STORAGE,
+        .name = "vertex_buffer"
+    });
+    auto indexBuffer = engine.device()->createBuffer({
+        .size = static_cast<u32>(indices.size() * sizeof(u32)),
+        .usage = canta::BufferUsage::STORAGE,
+        .name = "index_buffer"
+    });
+    auto primitiveBuffer = engine.device()->createBuffer({
+        .size = static_cast<u32>(primitives.size() * sizeof(u8)),
+        .usage = canta::BufferUsage::STORAGE,
+        .name = "primitive_buffer"
+    });
+    auto meshletBuffer = engine.device()->createBuffer({
+        .size = static_cast<u32>(meshlets.size() * sizeof(Meshlet)),
+        .usage = canta::BufferUsage::STORAGE,
+        .name = "meshlet_buffer"
+    });
+
+    uploadBuffer.upload(vertexBuffer, vertices);
+    uploadBuffer.upload(indexBuffer, indices);
+    uploadBuffer.upload(primitiveBuffer, primitives);
+    uploadBuffer.upload(meshletBuffer, meshlets);
+
 
     bool running = true;
     SDL_Event event;
@@ -45,7 +228,6 @@ int main() {
 
         engine.device()->beginFrame();
         engine.device()->gc();
-        auto flyingIndex = engine.device()->flyingIndex();
 
 
         {
@@ -72,13 +254,7 @@ int main() {
         }
 
 
-
-
         auto swapchainImage = swapchain->acquire();
-
-        commandPools[flyingIndex].reset();
-        auto& commandBuffer = commandPools[flyingIndex].getBuffer();
-        commandBuffer.begin();
 
         auto swapchainIndex = renderGraph.addImage({
             .handle = swapchainImage.value()
@@ -92,19 +268,6 @@ int main() {
 
         renderGraph.setBackbuffer(swapchainIndex);
         renderGraph.compile();
-        renderGraph.execute(commandBuffer);
-
-        commandBuffer.barrier({
-            .image = swapchainImage.value(),
-            .srcStage = canta::PipelineStage::COLOUR_OUTPUT,
-            .dstStage = canta::PipelineStage::BOTTOM,
-            .srcAccess = canta::Access::COLOUR_WRITE | canta::Access::COLOUR_READ,
-            .dstAccess = canta::Access::MEMORY_WRITE | canta::Access::MEMORY_READ,
-            .srcLayout = canta::ImageLayout::COLOUR_ATTACHMENT,
-            .dstLayout = canta::ImageLayout::PRESENT
-        });
-
-        commandBuffer.end();
 
         auto waits = std::to_array({
             { engine.device()->frameSemaphore(), engine.device()->framePrevValue() },
@@ -114,7 +277,8 @@ int main() {
             engine.device()->frameSemaphore()->getPair(),
             swapchain->presentSemaphore()->getPair()
         });
-        commandBuffer.submit(waits, signals);
+        renderGraph.execute(waits, signals, true);
+
         swapchain->present();
 
         engine.device()->endFrame();
